38a04f502457d20dba61e443db2b92486f84d00b
Prakrit Gajurel <96650932+DarkChungus@users.noreply.github.com>
2025-01-28 20:17:37 +0545

    Create Exercise 1.c

---
+++ Exercise 1.c
@@ -0,0 +1,26 @@
+// NOTE: Works only for small factorial numbers.
+
+#include <stdio.h>
+#include <conio.h>
+
+int factorial(int n){
+	int factorial = 1;
+	if(n==0){
+		return 1;
+	}
+	else{
+		for(int i=1; i<=n; i++){
+				factorial *= i;
+			}
+		return factorial;
+	}
+}
+
+int main(){
+	int userInput;
+	printf("\nEnter the number you want to see the factorial of: ");
+	fflush(stdout);
+	scanf("%d", userInput);
+	printf("The factorial of the number is %d.", factorial(userInput));
+	return 0;
+}
